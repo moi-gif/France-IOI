d741a603911cca1dcf921279617cba8788731764
serunos <72872185+serunos@users.noreply.github.com>
2021-02-03 09:08:10 +0100

    Update 02-Nombre d'amour.py
    Using proof by 9 to make the sum if it is over 10

--- Lvl-3/01-Gestion de caractères/02-Nombre d'amour.py
+++ Lvl-3/01-Gestion de caractères/02-Nombre d'amour.py
@@ -1,20 +1,11 @@
-enfant1,enfant2=input().split(" ")
-tabEnfant1=[0]*len(enfant1)
-tabEnfant2=[0]*len(enfant2)
-
-for i in range(len(enfant1)) :
-    tabEnfant1[i]=ord(enfant1[i])-65
-
-for i in range(len(enfant2)) :
-    tabEnfant2[i]=ord(enfant2[i])-65
-    
-nbEnfant1=sum(tabEnfant1)
-nbEnfant2=sum(tabEnfant2)
-
-while nbEnfant2>=10 or nbEnfant1>=10 :
-    tabChiffresEnfant1=map(int,str(nbEnfant1))
-    nbEnfant1=sum(tabChiffresEnfant1)
-    tabChiffresEnfant2=map(int,str(nbEnfant2))
-    nbEnfant2=sum(tabChiffresEnfant2)
-
-print(nbEnfant1,nbEnfant2)
+noms=input("Entrer les noms des deux enfants (séparer d'un espace)")
+noms=noms.split(" ")
+somme=0
+for i in range(2) :
+	somme=0
+	for k in range(len(noms[i])):
+		somme+=ord(noms[i][k])-65
+	if somme>10:
+		somme=somme%9
+	print(somme," ",end="")
+print()

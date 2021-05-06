b4f5b6db0e607de57fae57288e2a70733119c18e
moi-gif <83752668+moi-gif@users.noreply.github.com>
2021-05-06 09:32:37 +0200

    Update 02-Nombre d'amour.py

--- Lvl-3/01-Gestion de caractères/02-Nombre d'amour.py
+++ Lvl-3/01-Gestion de caractères/02-Nombre d'amour.py
@@ -1,9 +1,9 @@
 noms=input("Entrer les noms des deux enfants (séparés d'un espace)")
 noms=noms.split(" ")
 somme=0
-for i in range(2) :
+for loop in range(2) :
 	somme=0
-	for k in range(len(noms[i])):
+	for i in range(len(noms[loop])):
 		somme+=ord(noms[i][k])-65
 	if somme>10:
 		somme=somme%9
